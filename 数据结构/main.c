4f4023661f8545fb0dd9f03c336a90cb6ff4beb8
KangZH <1244565465@qq.com>
2020-01-20 01:16:50 +0800

    基本程序结构

---
+++ 数据结构/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  数据结构
+//
+//  Created by 康志恒 on 2020/1/20.
+//  Copyright © 2020年 kang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("main!\n");
+    return 0;
+}
---
+++ 数据结构/数据结构代码/linear/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  linear
+//
+//  Created by 康志恒 on 2020/1/20.
+//  Copyright © 2020年 kang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("linear\n");
+    return 0;
+}
